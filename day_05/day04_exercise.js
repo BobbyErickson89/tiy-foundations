761f2b16b9a47b0d3560360b9540edbc155dc623
Bobby Erickson <ericksonr07@gmail.com>
2016-12-04 19:52:09 -0500

    initial commit

---
+++ day_05/day04_exercise.js
@@ -0,0 +1,67 @@
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+// Q: What value is stored in `givenName` right now?
+// A:
+
+
+givenName = "Brett";
+// Q: What is `givenName` set to now?
+// A:
+
+
+givenName = givenName;
+// Q: What is `givenName` set to now?
+// A:
+
+
+
+/*=========================================================
+     Part 2: Simple Math
+*/
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A:
+
+math = high - "5";
+// Q: What is `math` set to?
+// A:
+
+
+// Using the `high` & `low` Variables, work through each of the 4 math operators below and log the answers to the console.
+// Write Code Below:
+
+
+
+
+
+/* =========================================================
+     Part 3: Expressions
+*/
+
+
+// Create a variable to calculate your age
+// The answer should read "NAME is XX years old"
+// The answer should not be written in a comment.
+var born = 1900;
+var today = 1900;
+
+// Answers Below:
+
+
+
+// Store some information following in variables.
+var yourName;
+var instructorName;
+
+// Update the variables above so the expression reads correctly.
+// Answers Below:
+
+
+// Final Statement
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
