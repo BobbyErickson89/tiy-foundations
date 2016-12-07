2501185d768e1f64bdca77f450c942fd2bedd040
Bobby Erickson <ericksonr07@gmail.com>
2016-12-07 17:32:51 -0500

    commented out my examples

--- day_06/day6_lecture.js
+++ day_06/day6_lecture.js
@@ -4,7 +4,7 @@
 //  objects act like a tool kit for creating interactive web pages.
 //
 // -We can view some of these using the built-in window object.
-console.log(window);
+// console.log(window);
 // -This window object contains other objects that represent the current browser or tab.
 // -It contains objects that model things like browser history and the device's screen.
 // -W3 Schools has some great documentation on what objects are available and what they
@@ -14,9 +14,9 @@ console.log(window);
 // -The DOM uses objects to create a representation of the current page.
 // -It creates a new object for each elements (and each individual section of
 //  text) within the page.
-console.log(window.document);
+// console.log(window.document);
 // OR
-console.log(document);
+// console.log(document);
 
 // When looking at the DOM, each part of it is called a Node.  There are four
 // main types of nodes.
@@ -40,19 +40,19 @@ console.log(document);
 // with much less typing.
 // -A JS library is pre-written JavaScript that allows for easier development
 // of javascript applications.
-var my_heading_js = document.getElementsByTagName('h1');
-console.log(my_heading_js);
+// var my_heading_js = document.getElementsByTagName('h1');
+// console.log(my_heading_js);
 // OR
-var my_heading_jquery = $('h1');
-console.log(my_heading_jquery);
+// var my_heading_jquery = $('h1');
+// console.log(my_heading_jquery);
 
 
 // ****** Selecting Elements ********
 // -Selecting HTML elements and classes will work the same way in jQuery as it
 //  does in CSS.  For example:
-var first_paragraph = $('.first-paragraph');
+// var first_paragraph = $('.first-paragraph');
 // OR
-var my_paragraphs = $('section p');
+// var my_paragraphs = $('section p');
 
 
 
@@ -62,34 +62,34 @@ var my_paragraphs = $('section p');
 // jQuery makes it simple to add and remove elements to the DOM.  jQuery has some
 // built-in functions that makes manipulating the DOM very easy.  .append() and
 // .prepend() allow us to add elements to the beginning or end of an element.
-var new_paragraph = "<p class='my-new-paragraph'>Here is my new paragraph</p>";
-$('section').append(new_paragraph);
+// var new_paragraph = "<p class='my-new-paragraph'>Here is my new paragraph</p>";
+// $('section').append(new_paragraph);
 
 // We are also able to remove elements with the .remove() function that comes with
 // jQuery.
-$('.my-new-paragraph').remove();
+// $('.my-new-paragraph').remove();
 
 
 // ******** Modifying Elements ********
 // Along with adding and removing elements, jQuery is also used to modify existing
 // elements.  Some commonly used functions are .addClass(), .removeClass()
-$('.second-section p').addClass('');
+// $('.second-section p').addClass('');
 // You can also go ahead and change defined CSS properties with jQuery
-var html_body = $('body')
-html_body.css('background-color', 'green');
+// var html_body = $('body')
+// html_body.css('background-color', 'green');
 
 
 // ******* jQuery Traversal *******
 // jQuery can also be used to search down the DOM.  For example:
-var my_sections = $('body').find('section');
-console.log(my_sections);
+// var my_sections = $('body').find('section');
+// console.log(my_sections);
 
 
 // ******* jQuery Events *******
 // jQuery can also be used to have functions happen when doing a certain event on
 // the page, like clicking.
-function changing_color(){
-  $('.second-section').append('<p>Here is a new paragraph</p>');
-}
-
-$('.event_button').click(changing_color);
+// function changing_color(){
+//   $('.second-section').append('<p>Here is a new paragraph</p>');
+// }
+//
+// $('.event_button').click(changing_color);
