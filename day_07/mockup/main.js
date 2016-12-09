4e5070506b751d568671168fb01e334f87965ec2
Bobby Erickson <ericksonr07@gmail.com>
2016-12-08 20:41:18 -0500

    adding day 7

---
+++ day_07/mockup/main.js
@@ -0,0 +1,52 @@
+window.onload = function(){
+  $('.size-text').hide();
+  $('.rating-text').hide();
+}
+
+$('a').click(function(e){
+  e.preventDefault();
+});
+
+
+function removeActiveClass(){
+  $('.navigation ul li a').removeClass('active');
+  $(this).addClass('active');
+}
+
+// Showing Size Tab
+function showSizeText(){
+  $('.size-text').show();
+  $('.product-text').hide();
+  $('.rating-text').hide();
+
+  removeActiveClass()
+}
+
+$('.size-tab').click(showSizeText);
+
+
+// Showing Product Tab
+function showProductText(){
+  $('.product-text').show();
+  $('.size-text').hide();
+  $('.rating-text').hide();
+
+  removeActiveClass()
+}
+
+$('.product-tab').click(showProductText);
+
+// Showing Rating Tab
+function showRatingText(){
+  $('.rating-text').show();
+  $('.size-text').hide();
+  $('.product-text').hide();
+
+  removeActiveClass()
+}
+
+$('.rating-tab').click(showRatingText);
+
+
+
+$('.section-1 p')
